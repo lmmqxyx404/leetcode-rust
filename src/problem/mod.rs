802cef1b554f4bbb796aaf6426bd7c486eecfa1e
lmmqxyx <2793037386@qq.com>
2022-03-07 15:05:03 +0800

    solve the p27

--- src/problem/mod.rs
+++ src/problem/mod.rs
@@ -1,4 +1,14 @@
+/*
+ * @Author: Lmmqxyx
+ * @Date: 2022-03-02 17:34:27
+ * @LastEditors: 
+ * @LastEditTime: 2022-03-04 13:54:54
+ * @FilePath: \leetcode-rust\src\problem\mod.rs
+ * @Description: 
+ */
 mod p0001_two_sum;
 mod p0004_median_of_two_sorted_arrays;
 mod p0013_roman_to_integer;
 mod p0026_remove_duplicates_from_sorted_array;
+
+mod p0027_remove_element;
