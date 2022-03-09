08cc6ae4c66c252ab4c77e2de67d27f4ec369dbd
lmmqxyx <2793037386@qq.com>
2022-03-09 14:48:39 +0800

    solve p2194

--- src/problem/mod.rs
+++ src/problem/mod.rs
@@ -18,3 +18,4 @@ mod p2160_minimum_sum_of_four_digit_number_after_splitting_digits;
 mod p2176_count_equal_and_divisible_pairs_in_an_array;
 mod p2180_count_integers_with_even_digit_sum;
 mod p0094_binary_tree_inorder_traversal;
+mod p2194_cells_in_a_range_on_an_excel_sheet;
