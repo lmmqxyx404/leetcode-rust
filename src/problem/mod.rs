556c4df33581f9e5b1feb65d46599975fb0b7c29
lmmqxyx <2793037386@qq.com>
2022-03-10 17:10:39 +0800

    solve p2164

--- src/problem/mod.rs
+++ src/problem/mod.rs
@@ -20,3 +20,4 @@ mod p2180_count_integers_with_even_digit_sum;
 mod p0094_binary_tree_inorder_traversal;
 mod p2194_cells_in_a_range_on_an_excel_sheet;
 mod p2190_most_frequent_number_following_key_in_an_array;
+mod p2164_sort_even_and_odd_indices_independently;
---
+++ src/problem/p2164_sort_even_and_odd_indices_independently.rs
@@ -0,0 +1,102 @@
+/**
+ * [2164] Sort Even and Odd Indices Independently
+ *
+ * You are given a 0-indexed integer array nums. Rearrange the values of nums according to the following rules:
+ * <ol>
+ * 	Sort the values at odd indices of nums in non-increasing order.
+ *
+ * 		For example, if nums = [4,<u>1</u>,2,<u>3</u>] before this step, it becomes [4,<u>3</u>,2,<u>1</u>] after. The values at odd indices 1 and 3 are sorted in non-increasing order.
+ *
+ *
+ * 	Sort the values at even indices of nums in non-decreasing order.
+ *
+ * 		For example, if nums = [<u>4</u>,1,<u>2</u>,3] before this step, it becomes [<u>2</u>,1,<u>4</u>,3] after. The values at even indices 0 and 2 are sorted in non-decreasing order.
+ *
+ *
+ * </ol>
+ * Return the array formed after rearranging the values of nums.
+ *  
+ * Example 1:
+ *
+ * Input: nums = [4,1,2,3]
+ * Output: [2,3,4,1]
+ * Explanation:
+ * First, we sort the values present at odd indices (1 and 3) in non-increasing order.
+ * So, nums changes from [4,<u>1</u>,2,<u>3</u>] to [4,<u>3</u>,2,<u>1</u>].
+ * Next, we sort the values present at even indices (0 and 2) in non-decreasing order.
+ * So, nums changes from [<u>4</u>,1,<u>2</u>,3] to [<u>2</u>,3,<u>4</u>,1].
+ * Thus, the array formed after rearranging the values is [2,3,4,1].
+ *
+ * Example 2:
+ *
+ * Input: nums = [2,1]
+ * Output: [2,1]
+ * Explanation:
+ * Since there is exactly one odd index and one even index, no rearrangement of values takes place.
+ * The resultant array formed is [2,1], which is the same as the initial array.
+ *
+ *  
+ * Constraints:
+ *
+ * 	1 <= nums.length <= 100
+ * 	1 <= nums[i] <= 100
+ *
+ */
+pub struct Solution {}
+
+// problem: https://leetcode.com/problems/sort-even-and-odd-indices-independently/
+// discuss: https://leetcode.com/problems/sort-even-and-odd-indices-independently/discuss/?currentPage=1&orderBy=most_votes&query=
+
+// submission codes start here
+
+impl Solution {
+    pub fn sort_even_odd(nums: Vec<i32>) -> Vec<i32> {
+        let mut odd: Vec<i32> = vec![];
+        let mut even: Vec<i32> = vec![];
+        for (index, val) in nums.iter().enumerate() {
+            if index % 2 == 0 {
+                even.push(*val);
+            } else {
+                odd.push(*val)
+            }
+        }
+        odd.sort();
+        even.sort();
+        odd.reverse();
+        let mut res: Vec<i32> = Vec::new();
+        let mut i = 0;
+        let mut j = 0;
+        let mut k = 0;
+        while k < nums.len() {
+            if k % 2 == 0 {
+                let tmp = (even[i]);
+                res.push(tmp.clone());
+                i += 1;
+            } else {
+                let tmp = (odd[j]);
+                res.push(tmp.clone());
+                j += 1;
+            }
+            k += 1;
+        }
+        res
+    }
+}
+
+// submission codes end
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_2164() {
+        /*
+        let mut res = Vec::new();
+        let a = &45;
+        res.push(*a);
+        assert_eq!(res[0], 45);
+        */
+        assert_eq!(Solution::sort_even_odd(vec![4, 1, 2, 3]), vec![2, 3, 4, 1]);
+    }
+}
